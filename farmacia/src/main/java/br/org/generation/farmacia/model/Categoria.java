022ad089a6a53db3aab8660baae1af48a87aa2af
unknown <biasa3s844@gmail.com>
2022-01-30 23:53:43 -0300

    Atualizando

--- farmacia/src/main/java/br/org/generation/farmacia/model/Categoria.java
+++ farmacia/src/main/java/br/org/generation/farmacia/model/Categoria.java
@@ -1,12 +1,17 @@
 package br.org.generation.farmacia.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -16,9 +21,37 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotBlank(message = "O campo CATEGORIA é obrigatório!")
-	@Size(min = 5)
-	private String categoria;
+	@NotBlank(message = "O campo DESCRIÇÃO é obrigatório!")
+	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
 	
 }
